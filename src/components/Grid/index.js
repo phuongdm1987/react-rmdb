f8394e8cb39501c7b8f73320f7b85ba5ff08defd
Henry - phuongdm <phuongdm@coccoc.com>
2021-11-25 15:59:21 +0700

    first commit

---
+++ src/components/Grid/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper, Content } from "./Grid.style";
+import PropTypes from 'prop-types';
+
+const Grid = ({ header, children }) => (
+    <Wrapper>
+        <h1>{header}</h1>
+        <Content>{children}</Content>
+    </Wrapper>
+)
+
+Grid.propTypes = {
+    header: PropTypes.string
+}
+
+export default Grid;
